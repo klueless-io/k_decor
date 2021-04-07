b72287e9718f184dd51233becbe02609e86d276c
klueless-io <david@ideasmen.com.au>
2021-04-07 21:39:02 +1000

    first commit

---
+++ lib/k_decor/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KDecor
+  VERSION = "0.1.0"
+end
---
+++ spec/k_decor_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KDecor do
+  it "has a version number" do
+    expect(KDecor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
